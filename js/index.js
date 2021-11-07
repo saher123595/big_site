dd739d79c06c0272ba236cf4a574f7b495ef41d9
saher_nasef <81627217+saher123595@users.noreply.github.com>
2021-11-07 22:57:10 +0200

    Delete js directory

--- js/index.js
+++
@@ -1,7 +0,0 @@
-let arrow = document.querySelector('.arrow');
-let navbar = document.querySelector('.navbar');
-window.addEventListener('scroll', () => {
-    navbar.classList.toggle('top', window.scrollY);
-    arrow.classList.toggle('right', window.scrollY);
-
-});
